adfc05fcae8fc3a88b5f44763a2444010f1aea9e
Jesus Castilla <jesus.jcg8@gmail.com>
2022-07-18 12:53:43 -0500

    pracitcando JS

---
+++ basics.js
@@ -0,0 +1,14 @@
+console.log("hello my buddies!")
+
+console.log(typeof("What's this?"))
+
+console.log(typeof(52))
+
+let sum = 1;
+
+console.log(sum)
+
+console.log(sum +1)
+
+const firstName = "Jesús";
+console.log(firstName)
